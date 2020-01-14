ad90c06ca33fe061bbef4ed3ad36bb807092545a
Anne Quiedeville <anne.quiedeville@orange.fr>
2020-01-14 15:22:27 +0100

    added axios and get data from api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,56 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
   
+  constructor(props) {
+    super(props)
+    this.state = {
+      items : [],
+    }
+  }
+
+  getItems() {
+    axios.get('http://127.0.0.1:8000/api/items')
+      .then(response => response.data)
+      .then(data => 
+          this.setState({
+            items : data['hydra:member'],
+          })
+        )
+  }
+
+  componentDidMount() {
+    this.getItems();
+  }
+
   render() {
+  
+    const{items} = this.state
+
+    const InputItem = () => {
+      return (
+        <div>
+          <label htmlFor="input-item">Produit</label>
+          <input type="text" id="input-item" />
+          <button>OK</button>
+        </div>
+      )
+    };
+
     return (
       <div className="App">
-        COUCOU
+        <div>
+          <InputItem/>
+        </div>
+        <ul>
+          {
+            items.map((item, i) => 
+              <li>{item.title}</li>
+            )
+            }
+        </ul>
       </div>
     );
   }
