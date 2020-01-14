cec4f60f0313ff9e4e5145b24907f6b356dc9e83
Anne Quiedeville <anne.quiedeville@orange.fr>
2020-01-14 16:14:34 +0100

    made to do list with checboxes and stuff

--- src/App.js
+++ src/App.js
@@ -9,14 +9,18 @@ class App extends Component {
     this.state = {
       items : [],
     }
+    this.handleProductInputChange = this.handleProductInputChange.bind(this)
+    this.handleButton = this.handleButton.bind(this)
+    this.handleCheckboxChange = this.handleCheckboxChange.bind(this)
   }
 
   getItems() {
-    axios.get('http://127.0.0.1:8000/api/items')
+    axios.get('http://127.0.0.1:8000/api/items?order[isChecked]')
       .then(response => response.data)
       .then(data => 
           this.setState({
             items : data['hydra:member'],
+            typingItem : null,
           })
         )
   }
@@ -25,16 +29,59 @@ class App extends Component {
     this.getItems();
   }
 
+  handleProductInputChange(event) {
+    this.setState({
+      typingItem : event.target.value,
+    })
+  }
+
+  handleButton(){
+    axios.post('http://127.0.0.1:8000/api/items', {
+      'title' : this.state.typingItem,
+      'isChecked' : false,
+      'category' : 'api/categories/3'
+    })
+      .then(response => {
+        this.getItems()
+        this.setState({
+          typingItem : null
+        })
+      })
+      .catch(function (error) {
+        console.log('error');
+      })
+  }
+
+  handleCheckboxChange(event) {
+    axios.put('http://127.0.0.1:8000/api/items/' + event.target.value, {
+      'title' : event.target.getAttribute('data-title'),
+      'isChecked' : event.target.checked,
+      'category': 'api/categories/3',
+    })
+      .then(response => {
+        this.getItems()
+      })
+  }
+
   render() {
   
     const{items} = this.state
+    const{typingItem} = this.state
 
     const InputItem = () => {
       return (
         <div>
           <label htmlFor="input-item">Produit</label>
-          <input type="text" id="input-item" />
-          <button>OK</button>
+          <input 
+            defaultValue={typingItem}
+            autoFocus
+            onChange={this.handleProductInputChange}
+            type="text" 
+            id="input-item" 
+          />
+          <button
+            onClick={this.handleButton}
+          >OK</button>
         </div>
       )
     };
@@ -47,7 +94,22 @@ class App extends Component {
         <ul>
           {
             items.map((item, i) => 
-              <li>{item.title}</li>
+              <li key={i}>
+                <input 
+                  type="checkbox"
+                  id={'check-' +i}
+                  onChange={this.handleCheckboxChange}
+                  data-title={item.title}
+                  value={item.id}
+                  checked={item.isChecked}
+                />
+                <label 
+                  htmlFor={'check-' +i}
+                  className={item.isChecked ? 'crossed' : ''}
+                >
+                  {item.title}
+                </label>
+              </li>
             )
             }
         </ul>
